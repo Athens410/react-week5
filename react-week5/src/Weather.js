93c1824e9a515f93a8338293a1bf728f00842230
Athens410 <athenah.coucean@gmail.com>
2023-05-30 18:17:55 -0400

    added border

--- react-week5/src/Weather.js
+++ react-week5/src/Weather.js
@@ -8,7 +8,52 @@ export default function weather (){
 
 
     return(
+        <div className="line">
+        <h1>London</h1>
+        <ul>
+         <li>
+            Monday 7:00
+         </li>
+         <li>
+          Mostly cloudy
+         </li>
 
-        <h1>test</h1>
+        </ul>
+         <div className="row">
+           <div className="col-6">
+            <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="mostly cloudy" />6°C
+
+
+           </div>
+           
+           
+           
+           
+           
+           <div className="col-6">
+            <ul>
+              <li> 
+            Precipitation: 2%
+              </li>
+              <li>
+            Humidity: 73%
+              </li>
+              <li>
+            Wind: 11 km/h
+              </li>
+
+
+            </ul>
+
+           </div>
+
+
+
+         </div>
+
+             
+             
+             
+             </div>
     );
 }
