23e276f71bada7b0580bd79203ded10a2b87e8cc
Athens410 <athenah.coucean@gmail.com>
2023-05-31 17:18:49 -0400

    updated

--- react-week5/src/WeatherInfo.js
+++ react-week5/src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props){
 
@@ -30,12 +31,17 @@ return(
         </ul>
          <div className="row">
            <div className="col-6">
-            <img src={props.data.iconUrl} alt={props.data.description} />
+          
             
-            
-            <span className="temperature">{Math.round(props.data.temperature)}</span>   <span className="unit">°C</span>
-
-
+              <WeatherTemperature
+    
+                 <WeatherIcon code={props.data.icon} size={52} />
+                                      
+                 
+                {""} {""}
+                
+                <span className="temperature">{Math.round(props.data.temperature)}</span>   <span className="unit">°C</span>
+                   
            </div>
            
            
